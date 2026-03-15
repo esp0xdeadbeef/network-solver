ddf184aa0dbf31680da40a9980f1b2c711143e87
esp0xdeadbeef <esp0xdeadbeef@gmail.com>
2026-03-15 23:32:08 +0100

    Fetch examples from network-labs repo and update flake inputs and scripts accordingly

--- compile-all-examples.sh
+++ compile-all-examples.sh
@@ -1,4 +1,3 @@
-# ./compile-all-examples.sh
 #!/usr/bin/env bash
 set -euo pipefail
 
@@ -8,8 +7,9 @@ cat >"$GIT_CONFIG_GLOBAL" <<'EOF'
 [safe]
   directory = *
 EOF
+example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
 
-find ../network-compiler/examples -type f -exec sh -c '
+find $example_repo/examples -type f -exec sh -c '
   printf "\n\n%s:\n\n" "$1"
   nix run .#compile-and-solve -- "$1" | jq -c
 ' _ {} \;
--- compile-one.sh
+++ compile-one.sh
@@ -2,4 +2,7 @@
 set -euo pipefail
 #nix run .#compile-and-solve -- ../network-compiler/examples/single-wan-with-nebula/inputs.nix
 #nix run .#compile-and-solve -- ../network-compiler/examples/priority-stability/inputs.nix
-nix run .#compile-and-solve -- ../network-compiler/examples/overlay-east-west/inputs.nix
+#nix run .#compile-and-solve -- ../network-compiler/examples/overlay-east-west/inputs.nix
+example_repo=$(nix eval --raw --impure --expr 'builtins.fetchGit { url = "git@github.com:esp0xdeadbeef/network-labs.git";}')
+
+nix run .#compile-and-solve -- $example_repo/examples/overlay-east-west/intent.nix
